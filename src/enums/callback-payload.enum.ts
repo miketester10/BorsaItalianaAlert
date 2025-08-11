3f832cdaf0afe48a152c98e4c15e570485076149
miketester10 <cadenza91@gmail.com>
2025-08-11 20:43:47 +0200

    Update FROM_CALLBACK_ALERTS_ATTIVI enum value for consistency
    - Changed the value of `FROM_CALLBACK_ALERTS_ATTIVI` in the `CallbackPayload` enum from "from_cb_a_t" to "from_cb_a_a" to ensure alignment with naming conventions and improve clarity in callback routing.

--- src/enums/callback-payload.enum.ts
+++ src/enums/callback-payload.enum.ts
@@ -1,6 +1,6 @@
 export enum CallbackPayload {
   ALL_ALERTS = "all_alerts",
   SINGLE_ALERT = "single_alert",
-  FROM_CALLBACK_ALERTS_ATTIVI = "from_cb_a_t",
+  FROM_CALLBACK_ALERTS_ATTIVI = "from_cb_a_a",
   FROM_COMANDO_PREZZO = "from_comando_prezzo",
 }
